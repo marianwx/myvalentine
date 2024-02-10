2152b441e0a675a0931337ad3d2c921b69b4fe98
marianwx <maransmk69@gmail.com>
2024-02-10 23:51:32 +0200

    .

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,8 +1,8 @@
 <template>
 
     <div class="h-screen flex flex-col justify-center items-center bg-gradient-to-br from-pink-200 via-rose-100 to-rose-200">
-      <div class="mb-20 flex justify-center">
-        <p class="text-3xl text-center bg-gradient-to-br from-pink-400 via-rose-400 to-pink-400 bg-clip-text text-transparent font-bold">Voudrais-tu être ma Valentine?</p>
+      <div class="mb-10 flex justify-center">
+        <p class="text-3xl text-center bg-gradient-to-br from-pink-400 via-rose-400 to-pink-400 bg-clip-text text-transparent font-bold">Vous devez vous connecter avec votre compte</p>
       </div>
       <img class="w-20 h-20 p-1 rounded-full shadow-md mb-3 ring-2 ring-gray-300 dark:ring-gray-500" src="https://cdn.discordapp.com/attachments/1205526275121348619/1205919217048617001/6A8542A6-8CAB-4425-805D-916480F500A2.jpg?ex=65da1eca&is=65c7a9ca&hm=2cb106be471b0e9920a607df5a2ad44771c445f4809f8663960caf7da808cf9f&" alt="Delia">
 
